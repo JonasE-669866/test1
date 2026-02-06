f2680b8e6e97fdc4e7771416f89ab2ac76679b68
Jonas <669866@stud.hvl.no>
2026-02-06 11:54:39 +0100

    add another mult

--- example.py
+++ example.py
@@ -12,9 +12,14 @@ def func2(a, b):
     return np.add(a, b)
 
 
+def func3(a, b):
+    return a*b
+
+
 if __name__ == "__main__":
     mul1 = 2
     mul2 = 3
 
     print(func(mul1, mul2))
     print(func2(mul1, mul2))
+    print(func3(mul1, mul2))
